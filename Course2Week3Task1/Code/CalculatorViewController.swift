8716a8d2f09cf581adfb43852b514b01920f3810
Evgeny <greatly01@ya.ru>
2020-06-22 10:18:04 +0500

    Итоговый результат

---
+++ Course2Week3Task1/Code/CalculatorViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CalculatorViewController.swift
+//  Course2Week3Task1
+//
+//  Copyright © 2018 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var operand1Label: UILabel!
+    @IBOutlet weak var operand2Label: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func stepperChangeValue(_ sender: UIStepper) {
+        let value = NumberFormatter()
+        value.minimumFractionDigits = 4
+        value.maximumFractionDigits = 4
+        operand1Label.text = value.string(from: NSNumber(value: sender.value))
+    }
+    
+    @IBAction func sliderChangeValue(_ sender: UISlider) {
+        let value = NumberFormatter()
+        value.minimumFractionDigits = 4
+        value.maximumFractionDigits = 4
+        operand2Label.text = value.string(from: NSNumber(value: sender.value))
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        guard let oper1 = Double(operand1Label.text!), let oper2 = Double(operand2Label.text!) else { return }
+        let value = NumberFormatter()
+        value.minimumFractionDigits = 0
+        value.maximumFractionDigits = 4
+        resultLabel.text = value.string(from: NSNumber(value: oper1 * oper2))
+    }
+    
+    override func viewDidLoad() {
+        stepper.layer.borderWidth = 2
+        stepper.layer.borderColor = UIColor(named: "Elements")?.cgColor
+        stepper.layer.cornerRadius = 6
+        stepper.layer.masksToBounds = true
+        super.viewDidLoad()
+    }
+}
